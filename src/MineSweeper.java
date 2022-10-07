41cff613c40f3987252f665e2551f516c305d3d4
Devin <devinhanson28@gmail.com>
2022-10-06 19:20:50 -0700

    Changed some issues with grid displaying and mine tracking. Still need to implement the hint portion of the program, and some GUI details like Field number and spacing.

--- src/MineSweeper.java
+++ src/MineSweeper.java
@@ -8,7 +8,7 @@
 import java.util.Scanner;
 
 /**
- * Steps:
+ * Steps (for development purposes):
  * - Create int[][]
  * - Read line in from input file
  * - Get data/location of mine from current line
@@ -21,28 +21,39 @@ public class MineSweeper {
     private int[][] hintGrid;
 
     public static void main(String[] args) {
+        boolean repeat = true;
         Scanner inputFile = new Scanner(System.in);
 
         String line = inputFile.nextLine();
         String[] nums = line.split(" ");
 
-        int cols = Integer.parseInt(nums[0]);
-        int rows = Integer.parseInt(nums[1]);
-
-        if(rows <= 100 && cols <= 100){
-            System.out.println("[" + rows + "]" +  ", " + "[" + cols + "]");
-            MineSweeper newGrid = new MineSweeper(rows, cols);
-            newGrid.setGrid();
-            System.out.println("length of grid = " + newGrid.hintGrid.length);
-            newGrid.outputGrids(newGrid.hintGrid);
-
-            for(int i = 0; i < rows; i++){
-                String[] currentLine = inputFile.nextLine().split("");
-
-                newGrid.findMines(currentLine, i);
+        while(repeat){
+            if(Integer.parseInt(nums[1]) == 0 || Integer.parseInt(nums[0]) == 0){
+                repeat = false;
             }
+            else if(Integer.parseInt(nums[1]) > 100 && Integer.parseInt(nums[0]) > 100){
+                throw new IndexOutOfBoundsException("Please enter a value between 1-100");
+            }
+            else if(Integer.parseInt(nums[1]) <= 100 && Integer.parseInt(nums[0]) <= 100
+            && Integer.parseInt(nums[1]) > 0 && Integer.parseInt(nums[0]) > 0){
+                System.out.println("[" + Integer.parseInt(nums[0]) + "]" +  ", " + "[" + Integer.parseInt(nums[1]) + "]");
+                MineSweeper newGrid = new MineSweeper(Integer.parseInt(nums[0]), Integer.parseInt(nums[1]));
+                System.out.println("num rows = " + newGrid.hintGrid.length);
+                System.out.println("num cols = " + newGrid.hintGrid[0].length);
+                newGrid.setGrid();
+                newGrid.outputGrids(newGrid.hintGrid);
 
-            newGrid.outputGrids(newGrid.hintGrid);
+                for(int i = 0; i < Integer.parseInt(nums[0]); i++){
+                    String[] currentLine = inputFile.nextLine().split("");
+
+                    newGrid.findMines(currentLine, i);
+                }
+
+                newGrid.outputGrids(newGrid.hintGrid);
+
+                line = inputFile.nextLine();
+                nums = line.split(" ");
+            }
         }
     }
 
@@ -65,9 +76,9 @@ public class MineSweeper {
 
         for(String str : currentRow){
             if(str.equals("*")){
-                System.out.println("[" + mineX + "]" +  ", " + "[" + mineY + "]");
-                hintGrid[mineX][mineY] = 9;
-                break;
+                System.out.println("Mine Found at: [" + mineX + "]" +  ", " + "[" + mineY + "]");
+                hintGrid[mineY][mineX] = 9;
+                mineX++;
             }else{
                 mineX++;
             }
@@ -75,7 +86,7 @@ public class MineSweeper {
 
     }
 
-    private void addHints(final int[][] hintGrid){
+    private void addHints(final int row, final int col){
 
     }
 
@@ -84,7 +95,7 @@ public class MineSweeper {
 
         for(int i = 0; i < hintGrid.length; i++){
             for(int j = 0; j < hintGrid[i].length; j++){
-                row.append(hintGrid[j][i]);
+                row.append(hintGrid[i][j]);
             }
             System.out.println(row.toString());
             row = new StringBuilder();
